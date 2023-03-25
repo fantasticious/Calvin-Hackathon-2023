bb997329d4a1d283707ae80768dac4861ffd4a42
Ael <bluebayband@gmail.com>
2023-03-25 09:32:27 -0400

    ver2

--- ott-zzal-al/src/app/app.component.ts
+++ ott-zzal-al/src/app/app.component.ts
@@ -5,6 +5,13 @@ import { initializeApp } from 'firebase/app';
 import { environment } from '../environments/environment';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
+interface possibleBottoms {
+  jeans: string[];
+  joggers: string[];
+  leggings: string[];
+  shorts: string[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,34 +19,44 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class AppComponent {
   top: Top | unknown;
+  chosenTopColor: string = '';
   topColors: string[] = [];
+  possibleBottoms: possibleBottoms | any;
+  possibleBottomTypes: string[] = [];
+  possibleBottomsAvailable: boolean = false;
+  possibleBottomColors: string[] = [];
+  bottomColorScheme: string[] = [];
+  rand6BottomColorScheme: string[] = [];
+
   bottom: Bottom | unknown;
+  chosenBottomColor: string = '';
   bottomColors: string[] = [];
 
   /* functions to manipulate state */
-  constructor(private db: AngularFirestore) {
-  }
+  constructor(private db: AngularFirestore) {}
 
   getTopColors = (top: string) => {
+    this.top = top;
     this.db
       .collection<Top>(`${top}`)
       .valueChanges()
       .subscribe((res) => {
         if (res) {
-          this.topColors = res.map((elem) => elem.color)
-          console.log("top colors: ", this.topColors)
+          this.topColors = res.map((elem) => elem.color);
+          console.log('top colors: ', this.topColors);
         }
       });
   };
 
   getBottomColors = (bottom: string) => {
+    this.bottom = bottom;
     this.db
       .collection<Bottom>(`${bottom}`)
       .valueChanges()
       .subscribe((res) => {
         if (res) {
-          this.bottomColors = res.map((elem) => elem.color)
-          console.log("bottom colors: ", this.bottomColors)
+          this.bottomColors = res.map((elem) => elem.color);
+          console.log('bottom colors: ', this.bottomColors);
         }
       });
   };
@@ -70,4 +87,28 @@ export class AppComponent {
         }
       });
   };
+
+  onChooseTopColor(color: string) {
+    this.chosenTopColor = color;
+    // console.log(this.chosenTopColor);
+    this.db
+      .doc<Top>(`${this.top}/${this.chosenTopColor}`)
+      .valueChanges()
+      .subscribe((matchList) => {
+        if (matchList) {
+          const {color, ...rest} = matchList;
+          this.possibleBottoms = rest;
+          this.possibleBottomTypes = Object.keys(this.possibleBottoms)
+          this.possibleBottomsAvailable = true;
+        }
+      });
+  }
+
+  onChooseBottomType = (bottomType: string): void =>  {
+    this.possibleBottoms[bottomType];
+    this.bottomColorScheme = this.possibleBottoms[bottomType];
+    this.rand6BottomColorScheme = this.bottomColorScheme.slice(0, 6);
+    console.log(this.rand6BottomColorScheme)
+  }
 }
+
