a1b5536df3c2fe449aba58a879ca1b2a7d22cadd
Ael <bluebayband@gmail.com>
2023-03-25 00:53:54 -0400

    add firebase

---
+++ ott-zzal-al/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'ott-zzal-al',
+    appId: '1:1045653893837:web:b067ecbca3d40517546f84',
+    storageBucket: 'ott-zzal-al.appspot.com',
+    apiKey: 'AIzaSyBxo9_22ANlZXu7CzMjZ55R0Oyuj_J0w3U',
+    authDomain: 'ott-zzal-al.firebaseapp.com',
+    messagingSenderId: '1045653893837',
+    measurementId: 'G-02EHWRC4SD',
+  },
+  production: true
+};
---
+++ ott-zzal-al/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'ott-zzal-al',
+    appId: '1:1045653893837:web:b067ecbca3d40517546f84',
+    storageBucket: 'ott-zzal-al.appspot.com',
+    apiKey: 'AIzaSyBxo9_22ANlZXu7CzMjZ55R0Oyuj_J0w3U',
+    authDomain: 'ott-zzal-al.firebaseapp.com',
+    messagingSenderId: '1045653893837',
+    measurementId: 'G-02EHWRC4SD',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
